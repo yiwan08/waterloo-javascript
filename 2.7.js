4037250b8bee72161c4a956fc4378f341dbbbf69
yiwanSven <69824146+yiwanSven@users.noreply.github.com>
2022-05-31 12:35:42 -0400

    Create 2.7.js

---
+++ 2.7.js
@@ -0,0 +1,28 @@
+// Flow of control.
+// Flow of control is the order in whcih the instructions are followed.
+
+// Variables
+// Declared during "var" statement.
+
+var cost;
+// variable cost is being declared.
+// variable value is undefined.
+
+var cost = 100;
+// variable cost is being initialized to 100.
+
+var cost = 100, taxRate = .15;
+// multiple variables can be declared and initialized on the same line.
+
+var cost = 100;
+var cost;
+// cost = 100 --> the value doesn't change if the variable has arleady been declared and initialized.
+
+// Constants
+// Share property of variables; however, not possible to redeclare.
+
+const cost = 100;
+// constant cost is being declared.
+
+// Identifiers
+// cost and taxRate are both identifiers.
