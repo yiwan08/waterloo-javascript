d17217c5aa863b037b4588431e325d49f96a96c0
yiwanSven <69824146+yiwanSven@users.noreply.github.com>
2022-05-31 13:20:57 -0400

    Strings

---
+++ 2.13.js
@@ -0,0 +1,17 @@
+// Escape Characters.
+
+/*
+\' == '
+\" == "
+\n == new line
+\t == tab
+\\ == \
+*/
+
+var first = "rain";
+var second = "bow";
+
+comsole.log(first + second);
+// "rainbow"
+console.log("What\'s this saying?");
+// "What's this saying?"
