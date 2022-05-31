3735c800730ca9421b9f92a7c37dc4228c5ca2f1
yiwanSven <69824146+yiwanSven@users.noreply.github.com>
2022-05-31 13:14:12 -0400

    Floating Point Numbers

---
+++ 2.10.js
@@ -0,0 +1,31 @@
+// Floating point numbers.
+
+// JavaScript stores all numbers as floating point numbers -- a finite representation.
+
+var x = Infinity, var y = -Infinity;
+// data type is number.
+
+// NaN = Not a Number
+
+console.log(1.23456789123456789123456789);
+// 1.234567891234568
+
+console.log(12.0);
+// 12
+console.log(12);
+// 12
+
+console.log(Infinity / -Infinity);
+// NaN
+console.log(7 / 0);
+// Infinity
+
+console.log(5.1 - 3.1);
+// 1.9999999999999996
+console.log(5 - 3);
+// 2
+
+console.log(50 % 7);
+// 1
+console.log(50.1 % 7);
+// 1.1000000000000014
