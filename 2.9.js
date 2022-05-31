2cc9561656d08b63ec134322963fab3afcff504b
yiwanSven <69824146+yiwanSven@users.noreply.github.com>
2022-05-31 12:42:48 -0400

    Types of Data

---
+++ 2.9.js
@@ -0,0 +1,21 @@
+// Types of data
+
+typeof() // return the type of data in String.
+
+console.log(typeof(5));
+// "number"
+
+console.log(typeof(5.0));
+// "number"
+
+console.log(typeof("5"));
+// "string"
+
+console.log(typeof(true));
+// "boolean"
+
+console.log(typeof(null));
+// "object"
+
+console.log(typeof(undefined));
+// "undefined"
