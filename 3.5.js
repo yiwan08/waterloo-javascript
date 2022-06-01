cb0adf19432fa26c7ff48c34eccbfa9019e58c6e
yiwanSven <69824146+yiwanSven@users.noreply.github.com>
2022-05-31 22:05:43 -0400

    Math Objects

---
+++ 3.5.js
@@ -0,0 +1,17 @@
+// Math objects
+
+console.log(Math.PI);
+// 3.141592653589793
+console.log(Math.floor(2.5));
+// 2 向下舍得整数
+console.log(Math.ceil(2.5));
+// 3 向上取得整数
+console.log(Math.max(1, 2, 3));
+// 3
+console.log(Math.pow(2, 3));
+// 8 (2 to the power of 3)
+console.log(Math.sqrt(4));
+// 2
+console.log(Math.floor(Math.random() * 10));
+// Pseudorandom Integer
+// Output random number within 0 and 10.
